78a4a775f83f45314eee3f3a60c6eb3569edb39c
Tony Le <tony@webinverters.com>
2015-09-22 18:34:14 -0400

    refactor waiting code out

--- src/fs-manager/index.js
+++ src/fs-manager/index.js
@@ -1,58 +1,34 @@
-var fs=require('fs');
 
-var writableStream;
+var fs=require('fs');
+var wait=require('../wait');
+
+
+
+var writableStream;//stub for stream to be initialized.
 
 function createStream(path){
   writableStream=fs.createWriteStream(path);
 }
 
 function writeLogEntry(data){
-  var tempId=id;
+  var tempId=wait.getId()
   writableStream.write(JSON.stringify(data)+"\n",function(err){
     if(err){
       //rejectWait(err)//perhaps application shouldn't break due to a logging error.
     }
-    resolveWait(tempId)
+    wait.resolveWait(tempId)
   });
 }
 
-//keep tracks of all log entries
-var id=0;
-var waiters={}
-function resolveWait(the_id){
-  waiters[the_id].done();
-}
-function rejectWait(err){
-  waiters[the_id].reject(err);
-}
 
 
-/**
- * returns a promise that resolves when entry is flushed.
- * @returns {*}
- */
-function waiter(){
-  var temp= p.defer()
-  waiters[id]={
-    done:function(){
-      temp.resolve(true)
-    },
-    error:function(err){
-      temp.reject(err)
-    }
-  };
-  if(id>2000111000) id=0;
-  return temp.promise;
-}
 
 var m={}
-m.waiter=waiter;
+m.waiter=wait.waiter;//track the current entry in the stream.
 m.writeLogEntry=function(data){
-  id++;
-  return writeLogEntry(data,id-1)
+  wait.incId()
+  return writeLogEntry(data)
 };
-
-
 
 
 module.exports=function(path,maxSize,maxCount){
