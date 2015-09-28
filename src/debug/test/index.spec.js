505038abf9812cbdb309d50c58b818529b637c54
Tony Le <tony@webinverters.com>
2015-09-28 11:42:41 -0400

    remove reference to folder

--- src/debug/test/index.spec.js
+++ src/debug/test/index.spec.js
@@ -10,7 +10,7 @@ describe('grab line of error',function(){
       .then(test)
       .catch(m)
       .then(function(result){
-        expect(result.file).to.include('win-with-logs/src/debug/test/index.spec.js');
+        expect(result.file).to.include('src/debug/test/index.spec.js');
         expect(result.line).to.equal('6');
         expect(result.func).to.equal('test')
       })
