77b4f55ce5c903ee360ccf965aad3e0ccdd38828
Tony Le <tony@webinverters.com>
2015-10-07 03:52:49 -0400

    fs int test

--- src/providers/filesystem/fs.int.js
+++ src/providers/filesystem/fs.int.js
@@ -1,8 +1,31 @@
 var m = require('./index');
-
+var exec = require('../../../test/helpers/exec')
+var fileConfig = require('../../data-structures').file_config;
+var fsTest = require('../../../test/helpers/checkFile');
 
 describe('fsProvider', function () {
-  xit('it works as expected.', function () {
-
-  })
+  var config;
+  beforeEach(function () {
+    config = {
+      app: "test",
+      env: "dev",
+      component: "testComponents",
+      logFilePath: './testing/',
+      maxLogFileSize: 100000,
+      maxLogFiles: 5
+    };
+    return exec("rm -rf testing;mkdir testing;")
+  });
+  afterEach(function () {
+    return exec("rm -rf testing;")
+  });
+  it('creates a new file and writes to the file.', function () {
+    var fsConfig = new fileConfig(config.logFilePath, config.maxLogFileSize, config.maxLogFiles);
+    var fsInstance = new m(fsConfig);
+    return fsInstance.write("hello")
+      .then(function () {
+        expect(fsTest.hasFile("./testing", "log0.log")).to.equal(true)
+        expect(fsTest.containLines('./testing/log0.log', ["hello"])).to.equal(true, "data should have been written to filesystem")
+      })
+  });
 });
