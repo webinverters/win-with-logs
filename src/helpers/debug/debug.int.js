428488b3d5efcfe5cb1b02c7e78ac41df5be72dc
Tony Le <tony@webinverters.com>
2015-10-07 03:28:01 -0400

    moved file

---
+++ src/helpers/debug/debug.int.js
@@ -0,0 +1,18 @@
+var m = require('./index');
+
+describe('grab line of error', function () {
+  it('return a formatted error message.', function () {
+    function test() {
+      throw new Error("hello")
+    }
+
+    return p.resolve()
+      .then(test)
+      .catch(m)
+      .then(function (result) {
+        expect(result.file).to.include('src/helpers/debug/debug.int.js');
+        expect(result.line).to.equal('6');
+        expect(result.func).to.equal('test')
+      })
+  })
+});
